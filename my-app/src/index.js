26b452bf2b260112e464b9b398bd14cae065c0c6
ghadban135 <ahmad135886@gmail.com>
2020-02-07 17:07:14 +0200

    sponsers react componenets

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,10 +9,11 @@ import Navbar from './Components/Common/Nav/nav';
 import Home1 from './Components/Home/firstPart/Home1';
 import Price from './Components/Modals/price slider/price';
 import Picture from './Components/Modals/picture slider/picture';
+import Sponsers from './Components/Home/secondpart/sponsers';
 
 
 
-ReactDOM.render(<Picture/>, document.getElementById('root'));
+ReactDOM.render(<Sponsers/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
